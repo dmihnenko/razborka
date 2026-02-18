5dea5cf9c48ad116302f210b2b46bffe9b04796a
TSP Developer <dev@tsp.local>
2026-02-18 02:28:41 +0200

    Исправлена кодировка файлов (v1.0.5)

--- src/components/Version.tsx
+++ src/components/Version.tsx
@@ -1,7 +1,7 @@
 ﻿export default function Version() {
   return (
     <div className="fixed bottom-2 right-2 z-50 text-xs text-gray-400 bg-white/50 px-2 py-1 rounded select-none pointer-events-none">
-      v1.0.4
+      v1.0.5
     </div>
   )
 }
